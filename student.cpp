4586585eff9f69b5918aedfb9cadfc32fd4b9165
Bart Lyons <lyons.bart@protonmail.com>
2021-06-14 18:07:37 -0400

    more work on the Student class.

--- student.cpp
+++ student.cpp
@@ -14,5 +14,34 @@ private:
 	int numberOfDaysToCompleteCourse[3]{0,0,0};
 	DegreeProgram degreeProgram = NOPROGRAM;
 public:
-
+	// Get
+	string getStudentID(){return studentID;}
+	string getFirstName(){return firstName;}
+	string getLastName(){return lastName;}
+	string getEmailAddress(){return emailAddress;}
+	int getAge() {return age;}
+	int * getNumberOfDaysToCompleteCourse() {
+		return numberOfDaysToCompleteCourse;
+	}
+	// Set
+	void setStudentID(string newStudentID) {
+		studentID = newStudentID;
+		return;
+	}
+	void setFirstName(string newStudentFirstName) {
+		firstName = newStudentFirstName;
+		return;
+	}
+	void setLastName(string newLastName) {
+		lastName = newLastName;
+		return;
+	}
+	void setEmailAddress(string newEmailAddress) {
+		emailAddress = newEmailAddress;
+		return;
+	} 
+	void setAge(int newAge) {
+		age = newAge;
+		return;
+	}
 };
